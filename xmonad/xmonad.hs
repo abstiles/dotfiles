b53b3026336f617c8956d6b9ce9464a97efb8aa5
Andrew Stiles <andrew.stiles@qlogic.com>
2012-09-11 14:43:11 -0500

    Add XMonad configuration

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,33 @@
+-- default desktop configuration for Fedora
+
+import System.Posix.Env (getEnv)
+import System.IO
+import Data.Maybe (maybe)
+
+import XMonad
+import XMonad.Config.Desktop
+import XMonad.Config.Gnome
+import XMonad.Config.Kde
+import XMonad.Config.Xfce
+import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.Run (spawnPipe)
+import XMonad.Actions.PhysicalScreens
+import qualified XMonad.StackSet as W
+
+main = do
+     session <- getEnv "DESKTOP_SESSION"
+     xmonad $ (maybe desktopConfig desktop session)
+	 { modMask = mod4Mask
+	 }
+	 `additionalKeys`
+	 [ ((mod4Mask, xK_Return), spawn "gnome-terminal")
+	 , ((mod4Mask, xK_l), spawn "gnome-screensaver-command -l")
+	 , ((mod4Mask .|. controlMask, xK_j), onNextNeighbour W.view)
+	 , ((mod4Mask .|. controlMask, xK_k), onPrevNeighbour W.view)
+	 ]
+
+desktop "gnome" = gnomeConfig
+desktop "kde" = kde4Config
+desktop "xfce" = xfceConfig
+desktop "xmonad-gnome" = gnomeConfig
+desktop _ = desktopConfig
