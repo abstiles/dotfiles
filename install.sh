31f43837159e7d1e732bba2b17875900fee4207d
Andrew Stiles <andrew.stiles@qlogic.com>
2012-12-18 10:52:04 -0600

    Support BSD in install script
    Replace references to "readlink -f" with a more cross-platform Python
    command. Remove use of GNU find's '-printf' action in favor of using sed
    to manipulate the printed path.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 usage () { cat << EOF
 Usage:
@@ -9,6 +9,11 @@ This script creates a symlink in \$HOME for each file in its local directory
 or for only those files that match the optional file glob(s) passed into
 this script.
 EOF
+}
+
+# Equivalent to "readlink -f" on Linux, but cross-platform (for BSD)
+get_real_path () {
+	echo $(python -c 'import os, sys; print os.path.realpath(sys.argv[1])' "$1")
 }
 
 files=()
@@ -26,7 +31,8 @@ if [ -z "$*" ]; then files=('*'); fi
 
 # Create all directories
 for item in "${files[@]}"; do
-	find $dir -type d ! -path "$dir/.*" -path "$dir/$item" -printf '%P\n' | \
+	find $dir -type d ! -path "$dir/.*" -path "$dir/$item" | \
+		sed -e "s|^$dir/||" | \
 		while read FILE; do
 			mkdir -p "$HOME/.$FILE"
 		done
@@ -34,11 +40,12 @@ done
 
 # Install all non-shell-script files as dotfiles in the home directory.
 for item in "${files[@]}"; do
-	find $dir -type f ! -path "$dir/.*" -path "$dir/$item" -printf '%P\n' | \
+	find $dir -type f ! -path "$dir/.*" -path "$dir/$item" | \
+		sed -e "s|^$dir/||" | \
 		grep -xv '.*\.sh' | \
 		while read FILE; do
 			# You'd think nested quotes like this wouldn't work, but it's the
 			# only way to get the full path grouped correctly (that I can see).
-			ln -s "$(readlink -f "$dir/$FILE")" "$HOME/.$FILE"
+			ln -s "$(get_real_path "$dir/$FILE")" "$HOME/.$FILE"
 		done
 done
