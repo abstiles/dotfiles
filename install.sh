0483b1ea9b17418dbbfe71c0b547571a9f43819d
Andrew Stiles <andrew.stiles@qlogic.com>
2012-12-18 10:38:16 -0600

    Fix issues with spaces in file names

--- install.sh
+++ install.sh
@@ -11,31 +11,34 @@ this script.
 EOF
 }
 
-# Check for presence of "help" option
-for arg in $@; do
+files=()
+for arg in "$@"; do
 	if [[ $arg == --help || $arg == -h ]]; then
 		usage
 		exit
 	fi
+	files+=("$arg")
 done
 
 dir=`dirname $0`
 
-if [ -z "$@" ]; then files='*'; else files="$@"; fi
+if [ -z "$*" ]; then files=('*'); fi
 
 # Create all directories
-for item in "$files"; do
-	find $dir -type d ! -path $dir/'.*' -path $dir/"$item" -printf '%P\n' | \
+for item in "${files[@]}"; do
+	find $dir -type d ! -path "$dir/.*" -path "$dir/$item" -printf '%P\n' | \
 		while read FILE; do
-			mkdir -p $HOME/.$FILE
+			mkdir -p "$HOME/.$FILE"
 		done
 done
 
 # Install all non-shell-script files as dotfiles in the home directory.
-for item in "$files"; do
-	find $dir -type f ! -path $dir/'.*' -path $dir/"$item" -printf '%P\n' | \
+for item in "${files[@]}"; do
+	find $dir -type f ! -path "$dir/.*" -path "$dir/$item" -printf '%P\n' | \
 		grep -xv '.*\.sh' | \
 		while read FILE; do
-			ln -s $(readlink -f $dir/$FILE) $HOME/.$FILE
+			# You'd think nested quotes like this wouldn't work, but it's the
+			# only way to get the full path grouped correctly (that I can see).
+			ln -s "$(readlink -f "$dir/$FILE")" "$HOME/.$FILE"
 		done
 done
